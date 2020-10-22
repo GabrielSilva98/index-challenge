4ed096a9409b362ab08e591d84194948aab46245
Psylocker <gabriel8198@gmail.com>
2020-10-22 09:17:46 -0300

    Script do abrir e fechar o modal

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-$( "#abc" ).click(function() {
-    alert( "Handler for .click() called." );
-  });
+    let cart = document.querySelector('.cart')
+    let miniCartModel = document.querySelector('.component-mini-cart')
+    let ModelClose = document.querySelector('#close')
+
+    cart.addEventListener('click', () => {
+        console.log(miniCartModel)
+        miniCartModel.style.display = 'block'
+    })
+
+    ModelClose.addEventListener('click', () => {
+        console.log(ModelClose)
+        miniCartModel.style.display = 'none'
+        })
+    
